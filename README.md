272dd36a902da28587c7486ea37696aadba52ea7
unknown <antonioleblanc@poli.ufrj.br>
2020-10-03 19:21:35 -0300

    first model

--- README.md
+++ README.md
@@ -1,1 +1,67 @@
-# models
+# Models 
+
+![pantera-model](docs/panteraModel.svg)
+
+# A) Usage on local machine
+
+
+## 1. Required programs to run the models
+
+ - [git](https://git-scm.com/downloads)
+ - [protoc](https://github.com/protocolbuffers/protobuf/releases)
+
+
+## 2. Install the tensorflow object detection api
+
+Acording to the [official docs](https://tensorflow-object-detection-api-tutorial.readthedocs.io/en/latest/install.html#tensorflow-object-detection-api-installation)
+
+Clone the tensorflow official repository
+```
+git clone https://github.com/tensorflow/models.git
+```
+
+Install with the commands
+
+```
+cd models/research
+protoc object_detection/protos/*.proto --python_out=.
+cp object_detection/packages/tf2/setup.py .
+python -m pip install .
+```
+
+## 3. Clone this repo
+
+```
+git clone https://github.com/si-tecnologia/models
+```
+
+# B) Usage with Docker
+
+DockerFile Example
+
+```
+FROM tensorflow/tensorflow:latest
+
+RUN apt install git protobuf-compiler -y
+
+RUN git clone --depth 1 https://github.com/tensorflow/models tf-models
+
+WORKDIR /tf-models/research/
+
+RUN protoc object_detection/protos/*.proto --python_out=.
+
+RUN cp object_detection/packages/tf2/setup.py .
+
+RUN python -m pip install .
+
+WORKDIR /app
+
+RUN git clone --depth 1 https://github.com/si-tecnologia/models
+
+COPY src/requirements.txt /
+
+RUN pip install -r /requirements.txt
+
+COPY src/ /app
+
+```
---
+++ smokeDetector_v0/model.py
@@ -0,0 +1,100 @@
+import base64
+import tensorflow as tf
+import numpy as np
+
+from io import BytesIO
+from PIL import Image
+from object_detection.utils import visualization_utils as viz_utils
+
+MODEL_PATH = 'smokeDetector_v0/fine_tuned_model/saved_model'
+
+class SmokeDetector:
+    '''Wrapper class of the Smoke Detection Model'''
+
+    detect_fn = tf.saved_model.load(MODEL_PATH)
+    
+    category_index = {
+        1: {'id': 1, 'name': 'Fumaça'}
+    }
+
+    def __init__(self, min_score_thresh=0.3):
+        self.min_score_thresh = min_score_thresh
+
+
+    def load_img_from_path(self, path):
+        image = Image.open(path)
+        return np.array(image, dtype='uint8')
+
+
+    def load_img_from_bytes(self, img_bytes):
+        image = Image.open(BytesIO(img_bytes))
+        return np.array(image, dtype='uint8')
+
+
+    def draw_detections(self, image_np, boxes, classes, scores):
+        """Wrapper function to visualize detections. 
+        Args:
+        image_np: uint8 numpy array with shape (img_height, img_width, 3)
+        boxes: a numpy array of shape [N, 4]
+        classes: a numpy array of shape [N]. Note that class indices are 1-based,
+            and match the keys in the label map.
+        scores: a numpy array of shape [N] or None.  If scores=None, then
+            this function assumes that the boxes to be plotted are groundtruth
+            boxes and plot all boxes as black with no classes or scores.
+        category_index: a dict containing category dictionaries (each holding
+            category index `id` and category name `name`) keyed by category indices.
+        """
+        viz_utils.visualize_boxes_and_labels_on_image_array(
+            image_np,
+            boxes,
+            classes,
+            scores,
+            self.category_index,
+            use_normalized_coordinates=True,
+            min_score_thresh=self.min_score_thresh)
+        return image_np
+
+
+    def predict(self, img, from_path=True):
+        '''Predicts the presence of smoke on the image
+        Args:
+        - img: the path to the image, OR the encoded img in bytes
+        - from_path: if set to False, the function reads the image in bytes
+        Return, a dict with the keys
+        - has_smoke::bool
+        - smoke_prob::float[0,1]
+        - detection_img::np.array -> image with the bounding box drawn (if there is smoke)
+        - rel_bbox::list -> the bounding box coordinates
+        '''
+
+        if from_path:
+            image_np = self.load_img_from_path(img)
+        else:
+            image_np = self.load_img_from_bytes(img)
+        
+        input_tensor = np.expand_dims(image_np, 0)
+        detections = self.detect_fn(input_tensor)
+        
+        smoke_prob = max(detections['detection_scores'][0].numpy())
+
+        if smoke_prob > self.min_score_thresh:
+
+            image_np_with_detections = image_np.copy()
+            draw_img = self.draw_detections(
+                image_np_with_detections,
+                detections['detection_boxes'][0].numpy(),
+                detections['detection_classes'][0].numpy().astype(np.uint32),
+                detections['detection_scores'][0].numpy())
+
+            return {
+                'has_smoke':True,
+                'smoke_prob':smoke_prob,
+                'detection_img': draw_img,
+                'rel_bbox':[0,0,0,0],
+            }
+        
+        else:
+            return {
+                'has_smoke':False,
+                'smoke_prob':smoke_prob
+                }
